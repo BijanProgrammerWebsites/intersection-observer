2e6723150ece5cf972e7618163254397b36b9af9
BijanProgrammer <bijaneisapour@gmail.com>
2021-12-23 20:59:31 +0330

    [ADD] home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ComponentsModule} from './components/components.module';
+import {PagesModule} from './pages/pages.module';
+import {PipesModule} from './pipes/pipes.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, ComponentsModule, PagesModule, PipesModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {
+    public demos: {title: string; link: string}[] = [{title: 'Random Trailer', link: 'random-trailer'}];
+}
